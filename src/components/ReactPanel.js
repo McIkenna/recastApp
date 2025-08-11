a10fa0948e307a19b589ae032602d7948047af51
ike0909 <52487212+McIkenna@users.noreply.github.com>
2025-08-11 17:06:06 -0500

    removed unwanted text

--- src/components/ReactPanel.js
+++ src/components/ReactPanel.js
@@ -9,7 +9,7 @@ const ReactPanel = ({
           <h3 className="text-lg font-medium">Selected Cube</h3>
            <p className="text-sm text-gray-500 mt-2">You will have to select color first before selecting cube for a color change</p>
           {!selectedInfo ? (
-            <p className="text-sm text-gray-500 mt-2">Click any cube in the canvas or select one from the list.</p>
+            <p className="text-sm text-gray-500 mt-2">Click any cube in the canvas</p>
           ) : (
                 <div className="text-sm text-gray-700 mb-1">
                     <h1>{selectedId}</h1>
